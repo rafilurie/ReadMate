5065b1e718e8e0ce27301499571e872582caef03
Alex Francis <afrancis@betterworks.com>
2015-07-11 11:12:28 -0700

    push up routes

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,19 @@
 from app import app, db
 from flask import render_template, request, jsonify, abort, Response, url_for
 
-@app.route('/')
+@app.route("/welcome")
 def index():
-    return "Hello world!"
+    return render_template("templates/index.html")
+
+# private, associated with the user
+@app.route("/photos")
+def photos():
+	return render_template("templates/photos.html")
+
+# private, associated with the user
+# as of 11:11am, going to associate chat with the detail view
+@app.route("/detail")
+def detail():
+	return render_template("templates/detail.html")
+
+
