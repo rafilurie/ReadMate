ab9937c6947bb12a6fc3240e463640275d12be32
Alex Francis <afrancis@betterworks.com>
2015-07-11 21:30:32 -0700

    added permissions

--- app/login.py
+++ app/login.py
@@ -6,20 +6,5 @@ from validate_email import validate_email
 from .models import User
 
 def enforce_password_requirements(password):
-    digits = re.search('[0-9]', password)
-    special_characters = re.search("[^A-Za-z0-9]", password)
-    uppercase = re.search("[A-Z]", password)
-    lowercase = re.search("[a-z]", password)
-    
-    if len(password) < 7:
-        return False
-    if not digits:
-        return all([uppercase, lowercase, special_characters])
-    if not special_characters:
-        return all([uppercase, lowercase, digits])
-    if not lowercase:
-        return all([uppercase, special_characters, digits])
-    if not uppercase:
-        return all([lowercase, digits, special_characters])
 
     return True
